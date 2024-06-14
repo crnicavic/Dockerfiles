188a858a183cd24593ad92820535fc6eb20935ff
crnicavic <cavic5000@tutanota.com>
2024-06-15 00:52:39 +0200

    adding x11 for using feh to view images

--- nchat/nchat.sh
+++ nchat/nchat.sh
@@ -1,3 +1,4 @@
-docker run -it --rm \
+docker run -it --rm -e DISPLAY=$DISPLAY \
 		   -v /home/user/.config/nchat:/home/nchat/.config/nchat:rw \
+		   -v /tmp/.X11-unix:/tmp/.X11-unix \
 		   nchat-whatsapp
