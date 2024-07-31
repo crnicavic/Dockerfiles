d050ccc1f8fe29e8ac78e85cda51e6a09af27f8b
crnicavic <cavic5000@tutanota.com>
2024-07-31 21:51:47 +0200

    Remove alsa-firefox since it's pointless

--- alsa-firefox/firefox.sh
+++
@@ -1,8 +0,0 @@
-docker run -it -e DISPLAY=$DISPLAY \
-						  -v /tmp/.X11-unix:/tmp/.X11-unix \
-						  -v /home/user/Downloads:/home/firefox/Downloads \
-						  --device /dev/snd \
-						  --env ALSA_CARD=Generic \
-						  --group-add=audio \
-						  --rm \
-						  firefox
