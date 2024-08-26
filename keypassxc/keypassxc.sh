b273cec036586c85979bc44da57708dcf615fc7c
crnicavic <cavic5000@tutanota.com>
2024-08-26 18:32:45 +0200

    Add KeePassXC password manager

---
+++ keypassxc/keypassxc.sh
@@ -0,0 +1,10 @@
+xhost local:docker
+
+wmname LG3D
+
+docker run -it \
+		   -e DISPLAY=$DISPLAY \
+		   -v /tmp/.X11-unix:/tmp/.X11-unix \
+		   -v keepassxc:/home/keepassxc/passdir \
+		   --rm \
+		   keepassxc
