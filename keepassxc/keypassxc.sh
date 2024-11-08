8a2ae8d79c018a736fc06ce2907d2a738a6a2b5c
crnicavic <cavic5000@tutanota.com>
2024-11-08 10:42:05 +0100

    added name to container

--- keepassxc/keypassxc.sh
+++ keepassxc/keypassxc.sh
@@ -6,6 +6,7 @@ docker run -it \
 		   -e DISPLAY=$DISPLAY \
 		   -v /tmp/.X11-unix:/tmp/.X11-unix \
 		   -v keepassxc:/home/keepassxc/passdir \
+		   --name keepassxc \
 		   --network none \
 		   --rm \
 		   keepassxc
