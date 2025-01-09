825d7fa95603a92d41f21ba5199bb1c705c9e7b8
crnicavic <cavic5000@tutanota.com>
2025-01-09 10:46:49 +0100

    open volume as read and write, cleanup

--- keepassxc/keepassxc.sh
+++ keepassxc/keepassxc.sh
@@ -3,10 +3,10 @@ xhost local:docker
 wmname LG3D
 
 docker run -it \
-		   -e DISPLAY=$DISPLAY \
-		   -v /tmp/.X11-unix:/tmp/.X11-unix \
-		   -v keepassxc:/home/keepassxc/passdir \
-		   --name keepassxc \
-		   --network none \
-		   --rm \
-		   keepassxc
+	   -e DISPLAY=$DISPLAY \
+	   -v /tmp/.X11-unix:/tmp/.X11-unix \
+	   -v "keepassxc:/home/keepassxc/passdir:rw" \
+	   --name keepassxc \
+	   --network none \
+	   --rm \
+	   keepassxc
