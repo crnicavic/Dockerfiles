1c1ebbbaf43ad2d3e31f51ee35baa8d11e4fc434
crnicavic <cavic5000@tutanota.com>
2025-04-01 18:42:11 +0200

    stop using volumes for keepassxc container because it's starting to get annoying

--- keepassxc/keepassxc.sh
+++ keepassxc/keepassxc.sh
@@ -5,7 +5,6 @@ wmname LG3D
 docker run -it \
 	   -e DISPLAY=$DISPLAY \
 	   -v /tmp/.X11-unix:/tmp/.X11-unix \
-	   -v "keepassxc:/home/keepassxc/passdir:rw" \
 	   --name keepassxc \
 	   --network none \
 	   --rm \
