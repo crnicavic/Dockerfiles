1b5417afcb13d7d9f13151b3ba9f342c9f62fa32
crnicavic <cavic5000@tutanota.com>
2025-07-12 18:42:36 +0200

    delete useless services

--- firefox/firefox.sh
+++
@@ -1,7 +0,0 @@
-docker run -it -e DISPLAY=$DISPLAY \
-						  -v /tmp/.X11-unix:/tmp/.X11-unix \
-						  -v /home/user/Downloads:/home/firefox/Downloads \
-						  --device /dev/snd \
-						  --name firefox \
-						  --rm \
-						  firefox
